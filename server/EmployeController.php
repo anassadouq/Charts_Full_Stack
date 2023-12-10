c2ee6d9a144521e779be198036bcecdea409e12a
anas sadouq <117522408+anassadouq@users.noreply.github.com>
2023-12-10 16:17:25 +0100

    Update EmployeController.php

--- server/EmployeController.php
+++ server/EmployeController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Employe;
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreEmployeRequest;
-use App\Http\Requests\UpdateEmployeRequest;
 
 class EmployeController extends Controller
 {
@@ -18,14 +16,15 @@ class EmployeController extends Controller
     {
         return response(Employe::create($request->all()));
     }
+
+    public function show(Employe $employe){
+        return response()->json([
+            'employe' => $employe
+        ]);
+    }
  
     public function update(Request $request, Employe $employe){
-        $request->validate([
-            'name'=>'required',
-            'salary' => 'required',
-            'age' => 'required',
-        ]);
-        $employe->fill($request->post())->update();
+        $employe->update($request->all());
         return response()->json([
             'message' => 'Item updated successfully'
         ]);
